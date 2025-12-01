e6629d0f5292ff149e6ca32deb53785fe5d416b3
Nicole Villena Motel <24-02494@g.batstate-u.edu.ph>
2025-12-01 19:24:33 +0800

    Remove redundant comments and clean up formatting

--- Product.java
+++ Product.java
@@ -58,7 +58,6 @@ public abstract class Product {
 
     /**
      * Adds stock to the product inventory
-     * @param amount The amount to add
      */
     public void restock(int amount) {
         if (amount > 0) {
@@ -68,8 +67,6 @@ public abstract class Product {
 
     /**
      * Consumes (sells) product from inventory
-     * @param amount The amount to consume
-     * @return true if successful, false if insufficient stock
      */
     public boolean consume(int amount) {
         if (amount <= 0) {
@@ -81,4 +78,5 @@ public abstract class Product {
         }
         return false;
     }
+
 }
